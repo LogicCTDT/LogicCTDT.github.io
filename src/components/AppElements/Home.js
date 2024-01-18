f5517445eaa317eb83acc49c042692883e4575a3
Ricky <logicirz@gmail.com>
2024-01-18 01:42:40 -0500

    fix small bug

--- src/components/AppElements/Home.js
+++ src/components/AppElements/Home.js
@@ -5,20 +5,9 @@ import { useState, useEffect } from 'react';
 
 
 const Home = () => {
-    const [contentHeight, setContentHeight] = useState(500);
-    const updateContentHeight = () => {
-      const content = document.getElementById('timeline-content');
-      if (content) {
-        setContentHeight(content.scrollHeight);
-      }
-    };
-    useEffect(() => {
-        
-      updateContentHeight();
-
-  }, []);
+    
     return(
-    <div id='rootnode' className={`text-center h-[${contentHeight}vh]`}>
+    <div id='rootnode' className={`text-center h-[500vh]`}>
       
     <Navbar />
     
